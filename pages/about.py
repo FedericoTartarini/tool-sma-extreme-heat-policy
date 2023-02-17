36572372aa7d92ce402bc1521f3a736c58e495e1
federico Tartarini <federicotartarini@gmail.com>
2023-02-17 13:33:25 +1100

    docs: updated about page

--- pages/about.py
+++ pages/about.py
@@ -5,14 +5,14 @@ import dash_bootstrap_components as dbc
 dash.register_page(__name__, title="About Page", name="About Page")
 
 body_text = """
-# Extreme Heat Policy
+# Sports and Medicine Australia - Extreme Heat Policy
 
 ## Scope
-This website in its current form provides recommendations only for the most popular sports played in Australia as defined in the 
-Extreme Heat Policy v1.0 2021, issued by Sport Medicine Australia.
+This website in its current form provides recommendations for the most popular sports played in 
+Australia as defined in the [Extreme Heat Policy v1.0 2021](https://www.baseballnsw.com.au/wp-content/uploads/2022/06/sma-extreme-heat-policy-2021-final.pdf), 
+[issued by Sport Medicine Australia](https://sma.org.au/sma-unveils-new-extreme-heat-policy/). 
 
 ## Introduction
-
 The [2021 SMA Extreme Heat Policy](https://sma.org.au/sma-site-content/uploads/2021/02/SMA-Extreme-Heat-Policy-2021-Final.pdf) 
 utilises the latest published research evidence to inform:
  * a biophysical model for predicting heat stress risk; 
@@ -35,17 +35,17 @@ coaches and sports medical teams responsible for the safety and wellbeing of peo
 in sport and physical activity in hot weather, as well as individuals wishing to manage heat
 stress risk during planned training activities.
 
-## Disclaimer
-The information in this policy is general. Reading or using this policy is not the same as
-getting medical advice from your doctor or health professional. All reasonable attempts have
-been made to ensure the information is accurate. However, SMA is not responsible for any
-loss, injury, claim or damage that may result from using or applying the information in this
-policy. The information in this policy should be considered and interpreted in the context of
-other risk management, insurance, governance and compliance frameworks and obligations
-relevant to sporting organisations. Familiarity with relevant International Sports Federation
-(ISF), National Sporting Organisation (NSO) and State Sporting Organisation (SSO) policies
-and requirements is essential to enable appropriate interpretation and application of the
-information in this policy.
+## Disclaimer and User Agreement
+This application is intended for informational and educational purposes only. 
+It visualizes and calculates heat stress risk for people playing sports based on meteorological data from [Norwegian Meteorological Institute](https://www.met.no/) 
+and information provided in the [Extreme Heat Policy v1.0 2021](https://www.baseballnsw.com.au/wp-content/uploads/2022/06/sma-extreme-heat-policy-2021-final.pdf), 
+[issued by Sport Medicine Australia](https://sma.org.au/sma-unveils-new-extreme-heat-policy/). 
+It is not intended to provide medical advice or replace professional medical judgment, diagnosis, or treatment. 
+The authors of this application do not take any responsibility for the accuracy, completeness, or reliability 
+of the information presented. Users should always consult with a qualified healthcare professional for any 
+questions regarding their health or medical conditions. By using this application, users acknowledge and 
+agree that they assume full responsibility for their use of the information provided and that the authors 
+of this application are not liable for any damages arising from the use or misuse of this application.
 
 ### Attributions
 #### Icons
