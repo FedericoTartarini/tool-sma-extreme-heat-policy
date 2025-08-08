b563c330fed554e493f606f9554b80d1ed8d6c89
federico tartarini <federicotartarini@gmail.com>
2025-08-08 17:13:23 +1000

    test(home): Add tests for visibility and location changes
    Implement tests to verify key elements on the home page are visible
    and ensure that changing the location updates the map and recommendations.
    Also, add a test for changing the country and its effect on the location
    dropdown.

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,31 +1,35 @@
+# tests/test_home.py
+
 from playwright.sync_api import Page, expect
 
-from my_app.my_classes import Defaults
+from my_app.my_classes import Defaults, IDs
 from my_app.utils import sports_category
 
 
 class TestHomePage:
-
-    def test_visibility_text(self, page: Page):
+    def test_visibility_text(self, page: Page) -> None:
+        """Verify key elements are visible and updating the location displays the map and recommendations."""
+        # Navigate to the home page
         page.goto("/")
-        expect(page.get_by_role("link", name="SMA Extreme Heat Policy")).to_be_visible()
-        expect(
-            page.locator("#id_sport").filter(has_text=Defaults.sport.value.upper())
-        ).to_be_visible()
-        expect(page.locator("#sport-image").get_by_role("img")).to_be_visible()
 
-        # change the location
-        page.locator("#id_postcode").click()
-        page.locator("#id_sport").get_by_role("combobox").fill("2205 arn")
-        page.locator("#id_sport").get_by_role("combobox").press("Enter")
+        # Check header link, default sport selection and sport image
+        expect(page.get_by_role("link", name="Extreme Heat Risk")).to_be_visible()
+        expect(page.locator("#id_sport")).to_be_visible()
+        expect(page.locator("#id-sport-image").get_by_role("img")).to_be_visible()
 
-        expect(page.locator("#map")).to_be_visible()
-        expect(page.get_by_role("img", name="Heat stress chart")).to_be_visible()
+        # Change the location
+        page.locator("#dropdown-location").click()
+        page.locator("#dropdown-location").type("2205 arn")
+        page.locator("#dropdown-location").press("Enter")
+
+        # Validate map and recommendations sections
+        expect(page.locator("#id-map")).to_be_visible()
         expect(page.get_by_role("heading", name="Key recommendations:")).to_be_visible()
         expect(page.get_by_role("button", name="Detailed suggestions:")).to_be_visible()
         expect(
             page.get_by_role("heading", name="Forecasted risk for today")
         ).to_be_visible()
+        expect(page.locator("#id-button-install")).to_be_visible()
         expect(
             page.get_by_role("link", name="Click here to provide your")
         ).to_be_visible()
@@ -34,10 +38,10 @@ class TestHomePage:
         page.goto("/")
         sports = sports_category.keys()
         drop_down_sport = Defaults.sport.value.upper()
-        for sport in sports:
-            page.locator("#id_sport").filter(has_text=drop_down_sport).click()
-            page.locator("#id_sport").get_by_role("combobox").fill(sport)
-            page.locator("#id_sport").get_by_role("combobox").press("Enter")
+        for sport in sports[:4]:
+            page.locator("#id-sport").filter(has_text=drop_down_sport).click()
+            page.locator("#id-sport").get_by_role("combobox").fill(sport)
+            page.locator("#id-sport").get_by_role("combobox").press("Enter")
             expect(page.get_by_role("img", name="Heat stress chart")).to_be_visible()
             drop_down_sport = sport
 
---
+++ tests/test_home_navigation.py
@@ -0,0 +1,58 @@
+from playwright.sync_api import Page, expect
+
+from my_app.my_classes import Defaults, IDs
+from my_app.utils import sports_category
+
+expect.set_options(timeout=2_000)
+
+
+class TestHomePage:
+    def test_visibility_text(self, page: Page) -> None:
+        """Verify key elements are visible and updating the location displays the map and recommendations."""
+        # Navigate to the home page
+        page.goto("/")
+
+        # Check header link, default sport selection and sport image
+        expect(page.get_by_role("link", name="Extreme Heat Risk")).to_be_visible()
+        expect(page.locator("#id_sport")).to_be_visible()
+        expect(page.locator("#dropdown-location")).to_be_visible()
+
+    def test_location_change(self, page: Page) -> None:
+        """Test changing the location updates the map and recommendations."""
+        # Navigate to the home page
+        page.goto("/")
+
+        # Change the location
+        page.locator("#dropdown-location").click()
+        page.locator("#dropdown-location").type("2205 arn")
+        page.locator("#dropdown-location").press("Enter")
+
+        # check that the location is updated
+        expect(page.get_by_text("Arncliffe, NSW, 2205")).to_be_visible()
+
+        # change back to default location
+        page.locator("#dropdown-location").click()
+        page.locator("#dropdown-location").type("2000 sydney")
+        page.locator("#dropdown-location").press("Enter")
+
+        # check that the location is updated
+        expect(page.get_by_text("Sydney, NSW, 2000")).to_be_visible()
+
+    def test_country_change(self, page: Page) -> None:
+        """Test changing the country updates the location dropdown."""
+        # Navigate to the home page
+        page.goto("/")
+
+        # Change the country
+        page.locator("#id-button-country").click()
+        page.locator("#modal-country-select-input").dblclick()
+        page.locator("#modal-country-select-input").type("Italy")
+        page.get_by_text("Italy").click()
+
+        # change back to default country
+        page.locator("#dropdown-location").click()
+        page.locator("#dropdown-location").type("budrio 40054")
+        page.locator("#dropdown-location").press("Enter")
+
+        # check that the location is updated
+        expect(page.get_by_text("Budrio")).to_be_visible()
