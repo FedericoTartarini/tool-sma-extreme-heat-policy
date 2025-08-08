23ec077387dd74bb98bfb3af5b9f5aca1e3a5775
federico tartarini <federicotartarini@gmail.com>
2025-08-08 17:18:53 +1000

    refactor(home): Simplify location dropdown handling
    Remove unused country parameter and streamline the location dropdown logic
    to enhance readability and maintainability. Update callback inputs to
    reflect changes in data flow.

--- pages/home.py
+++ pages/home.py
@@ -4,10 +4,13 @@ from urllib.parse import urlencode
 
 import dash
 import dash_mantine_components as dmc
-from dash import dcc, html
+from dash import dcc
 from dash.exceptions import PreventUpdate
+
+# from dash.exceptions import PreventUpdate
 from dash_extensions.enrich import Output, Input, State, Serverside, callback
 from firebase_admin import db
+from icecream import ic
 
 from components.current_risk import component_current_risk
 from components.detailed_recommendations import component_detailed_recommendation
@@ -21,14 +24,13 @@ from components.sport_image import (
     component_sport_image,
 )
 from config import URLS, Dropdowns, PostcodesDefault
-from my_app.my_classes import IDs, Defaults
+from my_app.my_classes import IDs
 from my_app.utils import (
     FirebaseFields,
     store_settings_dict,
     store_weather_risk_df,
     storage_user_id,
     get_weather_and_calculate_risk,
-    store_country,
 )
 
 ref = db.reference(FirebaseFields.database_name)
@@ -46,17 +48,13 @@ dash.register_page(
 def layout(
     id_sport=Dropdowns.SPORT.default,
     id_postcode=Dropdowns.LOCATION.default,
-    id_country=Defaults.country.value,
 ):
     return dmc.Stack(
         children=[
             dcc.Location(id="url", refresh=False),
             display_sport_dropdown(sport=id_sport),
             component_sport_image(),
-            html.Div(
-                display_location_dropdown(location=id_postcode, country=id_country),
-                id="dropdown-location",
-            ),
+            display_location_dropdown(location=id_postcode),
             component_map(),
             component_current_risk(),
             component_main_recommendation(),
@@ -70,16 +68,15 @@ def layout(
 
 @callback(
     Output("dropdown-location", "children"),
-    Input(store_country, "data"),
+    Input(IDs.modal_country_select, "value"),
+    State(store_settings_dict, "data"),
     prevent_initial_call=True,
 )
-def update_location_dropdown_country_change(country):
+def update_location_dropdown_country_change(country, settings):
     """Updates the location dropdown when the country is changed."""
     print("changed the country to", country)
     postcodes_default = PostcodesDefault()
-    return display_location_dropdown(
-        country=country, location=postcodes_default[country]
-    )
+    return display_location_dropdown(location=postcodes_default[country])
 
 
 @callback(
@@ -87,29 +84,23 @@ def update_location_dropdown_country_change(country):
     Output("url", "search"),
     State(store_settings_dict, "data"),
     State(storage_user_id, "data"),
-    Input(IDs.postcode, "value"),
+    Input(IDs.dropdown_location_value, "value"),
     Input(IDs.sport, "value"),
-    Input(store_country, "data"),
     prevent_initial_call=True,
 )
-def save_settings_in_storage_and_update_url(data, user_id, location, sport, country):
+def save_settings_in_storage_and_update_url(data, user_id, location, sport):
     """Saves in local storage the settings selected by the participant."""
     data = data or {}
     data[IDs.postcode] = location
     data[IDs.sport] = sport
-
-    if data[IDs.country] != country:
-        postcodes_default = PostcodesDefault()
-        data[IDs.postcode] = postcodes_default[country]
-
-    data[IDs.country] = country
-    print(data)
 
     firebase_data = deepcopy(data)
     if any(data.values()):
         firebase_data[FirebaseFields.user_id] = user_id
         firebase_data[FirebaseFields.timestamp] = time.time()
         ref.push().set(firebase_data)
+
+    ic(data, location, sport)
 
     url_data = {k: v for k, v in data.items()}
     # return the new values and the url
