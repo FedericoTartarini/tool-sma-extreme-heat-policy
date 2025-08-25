b1b2ee29e3a6d4646d5616a411f58d54b5596c5c
federico tartarini <federicotartarini@gmail.com>
2025-08-25 10:02:39 +1000

    Bump version: 1.1.1 → 1.1.2

--- my_app/footer.py
+++ my_app/footer.py
@@ -41,7 +41,7 @@ def my_footer():
                             
                             This website was reviewed by the Sports Medicine Australia Scientific Advisory Committee in 2025
                             
-                            Version: 1.1.1
+                            Version: 1.1.2
                             
                             [Contact Us](mailto:federico.tartarini@sydney.edu.au)
                             """
